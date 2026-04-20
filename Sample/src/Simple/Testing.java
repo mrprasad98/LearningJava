73cb384a00342f66ff9a20c51fa5a6813841d7ee
Rajendra <rajendraprasad.modem@gmail.com>
2026-04-20 22:33:14 +0530

    Testing file

---
+++ Sample/src/Simple/Testing.java
@@ -0,0 +1,14 @@
+package Simple;
+
+public class Testing {
+
+	public Testing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("RAJU");
+	}
+
+}
